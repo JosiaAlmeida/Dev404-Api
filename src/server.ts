81a228e3456828c03d954d8e45c5af59db3739ad
Josia Almeida <josiaalmeida0001@gmail.com>
2021-12-26 16:04:05 +0100

    Adicionando urlencoded

--- src/server.ts
+++ src/server.ts
@@ -17,6 +17,7 @@ const server = express()
 server.use(cors())
 
 server.use(express.json())
+server.use(express.urlencoded({extended: true}))
 
 server.use('/user', routerUser)
 server.use('/type', routerType)
