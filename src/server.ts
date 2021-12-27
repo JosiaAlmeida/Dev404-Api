5c86c5f2093fb0171c98bff9263535d919cd55e0
Josia Almeida <josiaalmeida0001@gmail.com>
2021-12-27 20:07:47 +0100

    Trocando runner

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,18 @@
+import express from 'express'
 import { NextFunction, Request, Response } from "express"
 
-require("dotenv/config")
+import "dotenv/config"
 // import './util/module-alias';
-const express = require('express')
-require('reflect-metadata')
-const cors = require('cors')
-// import routerUser from './Routes/userRoutes'
-// require('./database')
-// import routerType from './Routes/tipeRoutes'
-// import routerAparencia from './Routes/aparenciaRouter'
-// import routerQualidade from './Routes/qualidadeRouter'
-// import routerObjetivo from './Routes/objetivo'
-// import routerPacote from './Routes/pacoteRouter'
-// import routesUserActive from "./Routes/UserActiveRouter"
+import 'reflect-metadata'
+import cors from 'cors'
+import routerUser from './Routes/userRoutes'
+require('./database')
+import routerType from './Routes/tipeRoutes'
+import routerAparencia from './Routes/aparenciaRouter'
+import routerQualidade from './Routes/qualidadeRouter'
+import routerObjetivo from './Routes/objetivo'
+import routerPacote from './Routes/pacoteRouter'
+import routesUserActive from "./Routes/UserActiveRouter"
 
 const server = express()
 
@@ -21,13 +21,13 @@ server.use(cors())
 server.use(express.json())
 server.use(express.urlencoded({extended: true}))
 
-// server.use('/user', routerUser)
-// server.use('/type', routerType)
-// server.use('/aparencia', routerAparencia)
-// server.use('/qualidade', routerQualidade)
-// server.use('/objetivo',routerObjetivo)
-// server.use('/pacote', routerPacote)
-// server.use('/ActiveUser', routesUserActive)
+server.use('/user', routerUser)
+server.use('/type', routerType)
+server.use('/aparencia', routerAparencia)
+server.use('/qualidade', routerQualidade)
+server.use('/objetivo',routerObjetivo)
+server.use('/pacote', routerPacote)
+server.use('/ActiveUser', routesUserActive)
 server.get('/',(req, res)=>{
     res.send("Dev404-API")
 })
